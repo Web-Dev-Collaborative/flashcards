fca2fe7b4c36769b6fdd377667455a4f7214c836
usarneme <jamestcoop@gmail.com>
2019-04-14 10:34:04 -0700

    Reorg. Removed backend/ folder and put server.js in root. Updated import/usage links.

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.post('/register', function(req, res) {
 // Front End Static-Built Routes
 //
 // Pulls source from the production build folder
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, './build')))
 
 // All routes go to the Index.html file
 // The project uses React Router 4 to handle routing so it is not necessary to duplicate this in Express. 
 // RR4 will reroute to the appropriate URL as entered/navigated to within the app.
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'))
+  res.sendFile(path.join(__dirname, './build', 'index.html'))
 })
 
 // Your basic DBZ Port
